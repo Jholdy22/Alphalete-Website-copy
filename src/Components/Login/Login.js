cf3f757187f9c7ca07ba8f37f1acd1d052b4298f
Jace H <Jaceholdaway@gmail.com>
2018-09-13 15:59:33 -0600

    commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -7,7 +7,6 @@ class Login extends React.Component {
         let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
         
         window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri${url}&
-       
         response_type=code`
     }
 
