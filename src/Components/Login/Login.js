d275be6aa0c03bada25334e3caac4394b248626d
Jace H <Jaceholdaway@gmail.com>
2018-09-13 15:55:45 -0600

    auth0 possibly fixed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,7 +6,7 @@ class Login extends React.Component {
         let {REACT_APP_DOMAIN, REACT_APP_CLIENT_ID} = process.env;
         let url = `${encodeURIComponent(window.location.origin)}/auth/callback`
         
-        window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${url}&response_type=code`
+        window.location = `https://${REACT_APP_DOMAIN}/authorize?client_id=${REACT_APP_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri${url}&response_type=code`
     }
 
     render(){
